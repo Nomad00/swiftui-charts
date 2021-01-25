ec9c12b42ea0c69d082f95d743ffda024c6d6d7f
Mike Stanziano <mstanziano@community.com>
2021-01-25 10:23:11 -0500

    First pass fix for Package file issue

--- Package.swift
+++ Package.swift
@@ -3,17 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Charts",
+    name: "swiftui-charts",
     platforms: [
         .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
-        .library(name: "Charts", targets: ["Charts"])
+        .library(name: "swiftui-charts", targets: ["swiftui-charts"])
     ],
     dependencies: [
         .package(url: "https://github.com/swift-extensions/swiftui-shapes.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
-        .target(name: "Charts",dependencies: ["Shapes"])
+        .target(
+            name: "swiftui-charts",
+            dependencies: ["Shapes"],
+            path: "Sources"
+        )
     ]
 )
